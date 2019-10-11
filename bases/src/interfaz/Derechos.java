9c69d4483517ff997b26e7872111ccdffe17d302
FernandoSSilvaM <a01746925@itesm.mx>
2019-10-11 10:51:22 -0500

    First Upload

---
+++ bases/src/interfaz/Derechos.java
@@ -0,0 +1,132 @@
+package interfaz;
+
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JCheckBox;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+
+public class Derechos extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Derechos() {
+		setLayout(null);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Derecho 1");
+		chckbxNewCheckBox.setFont(new Font("Arial", Font.PLAIN, 12));
+		chckbxNewCheckBox.setBounds(6, 66, 93, 23);
+		add(chckbxNewCheckBox);
+		
+		JTextPane txtpnDerechos = new JTextPane();
+		txtpnDerechos.setFont(new Font("Arial", Font.BOLD, 14));
+		txtpnDerechos.setBackground(SystemColor.control);
+		txtpnDerechos.setText("Derechos Vulnerados");
+		txtpnDerechos.setBounds(10, 11, 158, 23);
+		add(txtpnDerechos);
+		
+		JCheckBox checkBox = new JCheckBox("Derecho 2");
+		checkBox.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox.setBounds(6, 92, 93, 23);
+		add(checkBox);
+		
+		JCheckBox checkBox_1 = new JCheckBox("Derecho 3");
+		checkBox_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_1.setBounds(6, 117, 93, 23);
+		add(checkBox_1);
+		
+		JCheckBox checkBox_2 = new JCheckBox("Derecho 4");
+		checkBox_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_2.setBounds(6, 143, 93, 23);
+		add(checkBox_2);
+		
+		JCheckBox checkBox_3 = new JCheckBox("Derecho 5");
+		checkBox_3.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_3.setBounds(6, 169, 93, 23);
+		add(checkBox_3);
+		
+		JCheckBox checkBox_4 = new JCheckBox("Derecho 6");
+		checkBox_4.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_4.setBounds(6, 195, 93, 23);
+		add(checkBox_4);
+		
+		JCheckBox checkBox_5 = new JCheckBox("Derecho 7");
+		checkBox_5.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_5.setBounds(6, 221, 93, 23);
+		add(checkBox_5);
+		
+		JCheckBox checkBox_6 = new JCheckBox("Derecho 8");
+		checkBox_6.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_6.setBounds(6, 248, 93, 23);
+		add(checkBox_6);
+		
+		JCheckBox checkBox_7 = new JCheckBox("Derecho 9");
+		checkBox_7.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_7.setBounds(6, 274, 93, 23);
+		add(checkBox_7);
+		
+		JCheckBox checkBox_8 = new JCheckBox("Derecho 10");
+		checkBox_8.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_8.setBounds(6, 300, 93, 23);
+		add(checkBox_8);
+		
+		JCheckBox checkBox_10 = new JCheckBox("Derecho 11");
+		checkBox_10.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_10.setBounds(272, 66, 93, 23);
+		add(checkBox_10);
+		
+		JCheckBox checkBox_11 = new JCheckBox("Derecho 12");
+		checkBox_11.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_11.setBounds(272, 92, 93, 23);
+		add(checkBox_11);
+		
+		JCheckBox checkBox_12 = new JCheckBox("Derecho 13");
+		checkBox_12.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_12.setBounds(272, 117, 93, 23);
+		add(checkBox_12);
+		
+		JCheckBox checkBox_13 = new JCheckBox("Derecho 14");
+		checkBox_13.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_13.setBounds(272, 143, 93, 23);
+		add(checkBox_13);
+		
+		JCheckBox checkBox_14 = new JCheckBox("Derecho 15");
+		checkBox_14.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_14.setBounds(272, 169, 93, 23);
+		add(checkBox_14);
+		
+		JCheckBox checkBox_15 = new JCheckBox("Derecho 16");
+		checkBox_15.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_15.setBounds(272, 195, 93, 23);
+		add(checkBox_15);
+		
+		JCheckBox checkBox_16 = new JCheckBox("Derecho 17");
+		checkBox_16.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_16.setBounds(272, 221, 93, 23);
+		add(checkBox_16);
+		
+		JCheckBox checkBox_17 = new JCheckBox("Derecho 18");
+		checkBox_17.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_17.setBounds(272, 248, 93, 23);
+		add(checkBox_17);
+		
+		JCheckBox checkBox_18 = new JCheckBox("Derecho 19");
+		checkBox_18.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_18.setBounds(272, 274, 93, 23);
+		add(checkBox_18);
+		
+		JCheckBox checkBox_19 = new JCheckBox("Derecho 20");
+		checkBox_19.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_19.setBounds(272, 300, 93, 23);
+		add(checkBox_19);
+		
+		JCheckBox checkBox_20 = new JCheckBox("Derecho 21");
+		checkBox_20.setFont(new Font("Arial", Font.PLAIN, 12));
+		checkBox_20.setBounds(272, 326, 93, 23);
+		add(checkBox_20);
+
+	}
+}
